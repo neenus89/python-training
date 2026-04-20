b50111450b13f62e59634e41ad803ddf39145654
Neenu <unni.neenu@gmail.com>
2026-04-20 18:11:02 +0530

    --Corrected second assignment of set

--- Assignment_set2.py
+++ Assignment_set2.py
@@ -1,9 +1,6 @@
 input_string = "Python is easy and Python is Powerful"
 string_set = set(input_string.split())
 
-unique_words = set()
-
-for word in string_set:
-    unique_words.add(word)
+unique_words = set(string_set)
 
 print("Unique words:", unique_words)
