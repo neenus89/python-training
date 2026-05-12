93ea5d4f2e2e459174ee99dc00b789f2f602dfbb
Neenu <unni.neenu@gmail.com>
2026-05-12 13:05:30 +0530

    --Corrected assigment 3 in try except

--- try_expect/Assignment_3.py
+++ try_expect/Assignment_3.py
@@ -1,19 +1,20 @@
 def convert_string_to_first_letter_upper(input_string):
-    try:
         if not input_string.isalpha():
             raise TypeError()
         result = input_string.capitalize()
         return result
-    except TypeError:
-        print("Input string contains numbers. Please enter a valid string.")
-    except:
-        print("An error occurred while processing the string.")
 
 user_input = input("Enter a string: ")
-converted_string = convert_string_to_first_letter_upper(user_input)
 
-if converted_string:
+try:
+    converted_string = convert_string_to_first_letter_upper(user_input)
     print("Converted string:", converted_string)
+except TypeError:
+    print("Input string contains numbers. Please enter a valid string.")
+except:
+    print("An error occurred while processing the string.")
+
+    
     
     
     
